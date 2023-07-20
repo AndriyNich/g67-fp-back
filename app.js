a4910ab13961a07f029839bf29bcedd9f8f73431
AndriyNich <111660438+AndriyNich@users.noreply.github.com>
2023-07-20 18:17:42 +0300

    start work with swagger

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const logger = require("morgan");
-const { pathToFile } = require("./constants");
+// const { pathToFile } = require("./constants");
 
 const { authRouter } = require("./routes/api");
 
 const app = express();
+
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const formatLog = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatLog));
 app.use(cors());
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/api/users", authRouter);
 
