7b0cf1e89e4af3c717ff182b1fab84f3cb2afc34
AndriyNich <111660438+AndriyNich@users.noreply.github.com>
2023-07-24 12:06:54 +0300

    feature notices
    replace imlementation for notices list on agregation

--- controllers/notice/getList.js
+++ controllers/notice/getList.js
@@ -1,37 +1,37 @@
-const { Notice } = require("../../models/notice");
+const { Notice } = require('../../models/notice');
 
 const getList = async (req, res) => {
-  const { page = 1, limit = 20, title = "", category = "" } = req.query;
+  const { page = 1, limit = 20, title = '', category = '' } = req.query;
   const skip = (page - 1) * limit;
 
   let queryString = {};
   if (title) {
-    queryString = { ...queryString, title: { $regex: title, $options: "i" } };
+    queryString = { ...queryString, title: { $regex: title, $options: 'i' } };
   }
 
   if (category) {
     queryString = { ...queryString, category };
   }
 
-  const result = await Notice.find(queryString, "-createdAt -updatedAt", {
-    skip,
-    limit,
-  });
+  const result = await Notice.aggregate([
+    { $match: queryString },
+    {
+      $facet: {
+        totalCount: [{ $count: 'count' }],
+        notices: [{ $skip: Number(skip) }, { $limit: Number(limit) }],
+      },
+    },
+    {
+      $project: {
+        totalCount: { $arrayElemAt: ['$totalCount.count', 0] },
+        page: page,
+        perPage: limit,
+        notices: 1,
+      },
+    },
+  ]);
 
-  const maxHits = await Notice.find(queryString).count();
-
-  // const ddd = await Notice.aggregate([
-  //   { $match: { title: { $regex: "dog", $options: "i" } } },
-  //   { $group: { _id: { $sum: 1 } } },
-  // ]);
-
-  const data = JSON.parse(JSON.stringify(result)).map((e) => {
-    return { ...e, favorite: false };
-  });
-
-  const responseData = { notices: data, maxHits };
-
-  res.json(responseData);
+  res.json(result);
 };
 
 module.exports = getList;
