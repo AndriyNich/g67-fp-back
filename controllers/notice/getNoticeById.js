be730c7b3ad4016dbeb4d482cc3f26f475635673
AndriyNich <111660438+AndriyNich@users.noreply.github.com>
2023-07-25 11:46:50 +0300

    fixed notices
    change route api/notices/:id add user info: phone & email

--- controllers/notice/getNoticeById.js
+++ controllers/notice/getNoticeById.js
@@ -1,16 +1,29 @@
-const { Notice } = require("../../models/notice");
-const { HttpError } = require("../../helpers");
+const { Notice } = require('../../models/notice');
+const { User } = require('../../models/user');
+const { HttpError } = require('../../helpers');
 
 const getNoticeById = async (req, res) => {
   const { id } = req.params;
 
-  const result = await Notice.findById(id, "-createdAt -updatedAt");
-  if (!result) {
-    throw HttpError(404, "Not found");
+  const notice = await Notice.findById(id, '-createdAt -updatedAt');
+  if (!notice) {
+    throw HttpError(404, 'Not found');
   }
-  const data = { ...JSON.parse(JSON.stringify(result)), favorite: false };
+  const dataNotice = { ...JSON.parse(JSON.stringify(notice)), favorite: false };
 
-  res.json(data);
+  console.log(dataNotice.owner);
+
+  const _id = dataNotice.owner;
+  const user = await User.findById(_id, 'phone email');
+  if (!user) {
+    throw HttpError(404, 'Not found');
+  }
+
+  const dataUser = JSON.parse(JSON.stringify(user));
+
+  const result = { notice: dataNotice, user: dataUser };
+
+  res.json(result);
 };
 
 module.exports = getNoticeById;
