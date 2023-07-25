497ffa71a333b5738fb04ce165e4d13eb0434dbb
AndriyNich <111660438+AndriyNich@users.noreply.github.com>
2023-07-25 15:35:10 +0300

    chek message error

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
 });
 app.use((err, req, res, next) => {
-  const { status = 500, message = 'Server errpr' } = err;
+  const { status = 500, message = 'Server error' } = err;
   res.status(status).json({ message });
 });
 
