0865175f98d7e8cb8bbe1240281a4dd9a888d17a
AndriyNich <111660438+AndriyNich@users.noreply.github.com>
2023-07-27 17:30:49 +0300

    fixed error

--- helpers/HttpError.js
+++ helpers/HttpError.js
@@ -1,8 +1,6 @@
 const errorMessageList = require('../constants');
 
 const HttpError = (status, message = errorMessageList[status]) => {
-  console.log(`status: ${status}, message: ${message}`);
-
   const error = new Error(message);
   error.status = status;
   return error;
