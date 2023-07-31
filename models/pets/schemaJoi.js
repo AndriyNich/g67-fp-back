1af005308bba13d0880fc68cd72663b0657abb93
AndriyNich <info@bembi.ua>
2023-07-31 23:34:20 +0300

    fixed pets
    scheme Joi field comments allow empty

--- models/pets/schemaJoi.js
+++ models/pets/schemaJoi.js
@@ -1,10 +1,10 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 const addSchema = Joi.object({
   name: Joi.string().min(2).max(16).required(),
   birthday: Joi.string(),
   type: Joi.string().min(2).max(16),
-  comments: Joi.string().max(120),
+  comments: Joi.string().max(120).empty(""),
 });
 
 module.exports = {
