c560a13341a63106adcb0335a8a8c66c2f4f54d7
AndriyNich <info@bembi.ua>
2023-07-31 21:20:00 +0300

    feature user
    Added a check when changing the email to ensure it is not being used by another user.

--- controllers/auth/updateUser.js
+++ controllers/auth/updateUser.js
@@ -1,9 +1,21 @@
 const { User } = require("../../models/users");
 
+const { HttpError } = require("../../helpers");
+
 const updateUser = async (req, res) => {
   const { user } = req;
 
   let updateUser = Object.assign(user, req.body);
+
+  if (user?.email) {
+    const otherUserWithNewEmail = await User.findOne({
+      email: user.email,
+      _id: { $ne: user._id },
+    });
+    if (otherUserWithNewEmail !== null) {
+      throw HttpError(409, "Email already in use other user");
+    }
+  }
 
   const avatarURL = req?.file?.path;
   if (avatarURL) {
